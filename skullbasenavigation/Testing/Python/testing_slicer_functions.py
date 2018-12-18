2440911531a51470e8b34a2326249cadbfbb7ad3
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-12-18 12:11:47 +0000

    Add docstrings to tests
    We currently have two sets of test descriptions: the docstrings
    and the messages that are printed. Perhaps we should sync them
    or do away with the printed messages altogether.

--- skullbasenavigation/Testing/Python/testing_slicer_functions.py
+++ skullbasenavigation/Testing/Python/testing_slicer_functions.py
@@ -10,6 +10,7 @@ import sbn.functions as fns
 
 
 class TestOpenIGTLinkConnection(unittest.TestCase):
+    """A test case for checking the connection through OpenIGTLink."""
 
     def setUp(self):
         self.message = "Creating and testing an OpenIGTLink connection"
@@ -21,11 +22,13 @@ class TestOpenIGTLinkConnection(unittest.TestCase):
                                                 self.port)
 
     def test_connect_to_OpenIGTLink(self):
+        """Check that we connect via OpenIGTLink correctly."""
         print(self.message)
         self.assertEqual(self.cnode.GetServerHostname(), self.host)
 
 
 class TestNeedle(unittest.TestCase):
+    """A base class for test cases requiring a needle model."""
     @classmethod
     def setUpClass(cls):
         cls.message = "Creating and testing a Needle model"
@@ -40,21 +43,24 @@ class TestNeedle(unittest.TestCase):
 
 
 class TestCreateNeedleModel(TestNeedle):
-
+    """A test case for creating a needle model."""
     def runTest(self):
+        """Check that we can create a needle and set its name correctly."""
         print(self.message)
         self.assertEqual(self.needle.GetName(), self.needle_name)
 
 
 class TestNodeVisibilitySettings(TestNeedle):
-
+    """A test case for setting the visibility of nodes."""
     def test_visible_setting(self):
+        """Check that we can make a node visible."""
         self.message = "Testing node visible setting"
         print(self.message)
         fns.set_node_visible(self.needle)
         self.assertEqual(self.needle.GetDisplayVisibility(), 1)
 
     def test_invisible_setting(self):
+        """Check that we can make a node invisible."""
         self.message = "Testing node invisible setting"
         print(self.message)
         fns.set_node_invisible(self.needle)
@@ -62,8 +68,9 @@ class TestNodeVisibilitySettings(TestNeedle):
 
 
 class TestGetItemId(TestNeedle):
-
+    """A test case for the behaviour of get_item_id_by_name."""
     def test_get_item_id_NSinput(self):
+        """Check that we cannot get an item with a non-string name."""
         self.message = "Testing getting the item id with \
                         invalid (non-string) input"
         print(self.message)
@@ -72,12 +79,14 @@ class TestGetItemId(TestNeedle):
         self.assertEqual(item_id, -1)
 
     def test_get_item_id_valid_input(self):
+        """Check that we can get an item with a valid name."""
         self.message = "Testing if item exists from node with valid input"
         print(self.message)
         item_id = fns.get_item_id_by_name(self.needle.GetName())
         self.assertEqual(fns.check_if_item_exists(item_id), True)
 
     def test_get_item_id_NEinput(self):
+        """Check that we cannot get an item with a non-existent name."""
         self.message = "Testing if item exists from node with invalid \
                         (non-existent) input"
         print(self.message)
@@ -87,6 +96,7 @@ class TestGetItemId(TestNeedle):
 
 
 class TestTransform(TestNeedle):
+    """A test case for transform-related functions."""
     @classmethod
     def setUpClass(cls):
         super(TestTransform, cls).setUpClass()
@@ -96,6 +106,7 @@ class TestTransform(TestNeedle):
         cls.transform_id = fns.get_item_id_by_name(cls.transform_name)
 
     def test_creating_transform(self):
+        """Check that we can create a transform."""
         self.message = "Testing Transform Creation"
         print(self.message)
         self.assertEqual(self.transform.GetName(), self.tf_name)
@@ -103,12 +114,14 @@ class TestTransform(TestNeedle):
                          type(slicer.vtkMRMLLinearTransformNode()))
 
     def test_actual_transform(self):
+        """Check that we correctly identify a transform as such."""
         self.message = "Test if actual transform"
         print(self.message)
         self.assertEqual(fns.check_if_item_is_transform(self.transform_id),
                          True)
 
     def test_is_node_transform_wrapper(self):
+        """Check the transform checker wrapper function."""
         self.message = "Testing Transform Checker Wrapper Function"
         print(self.message)
         self.assertEqual(
@@ -116,6 +129,7 @@ class TestTransform(TestNeedle):
             True)
 
     def test_not_transform(self):
+        """Check that we do not mistake non-transforms for transforms."""
         self.message = "Test not a transform"
         print(self.message)
         item_id = fns.get_item_id_by_name(self.needle.GetName())
@@ -123,13 +137,14 @@ class TestTransform(TestNeedle):
 
 
 class TestTransformFiliation(unittest.TestCase):
-
+    """A test case for node hierarchies."""
     def setUp(self):
         self.message = "Testing node hierachy"
         self.tf_child = fns.create_linear_transform_node('child')
         self.tf_parent = fns.create_linear_transform_node('parent')
 
     def test_node_hierarchy(self):
+        """Check that we can correctly set a transform's parent."""
         print(self.message)
         fns.set_parent_of_transform_hierarchy_node(self.tf_child,
                                                    self.tf_parent)
