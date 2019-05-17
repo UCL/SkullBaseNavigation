bab34274cb6348de2887ddb72d07f1af507f58af
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2019-05-17 12:43:43 +0100

    Update Slicer launch command#

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-"/c/Program Files/Slicer 4.8.1/Slicer.exe" --python-script /c/Users/Tom/Documents/WEISS/Code/Python/skullbasenavigation/skullbasenavigation/sbn_slicelet.py
+"/c/Program Files/Slicer 4.8.1/Slicer.exe" --python-script /c/Users/SBN/Code/skullbasenavigation/skullbasenavigation/sbn_slicelet.py
