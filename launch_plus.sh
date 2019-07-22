fe4ed9a32c37152dd27097ca2983e5df170f664f
Thomas Dowrick <t.dowrick@ucl.ac.uk>
2019-07-22 14:48:07 +0100

    Killing remaing servers before relaunching

--- launch_plus.sh
+++ launch_plus.sh
@@ -1,2 +1,7 @@
+plus_servers=$(ps aux | grep -ic plus)
+if [[ $plus_servers != 0 ]]; then
+    echo "Killing $plus_servers PLUS servers"
+    kill -9 $(ps aux | grep -i plus | awk '{print $1}' )
+fi
 /c/Users/SBN/PlusApp-2.6.0.20190221-StealthLink-Win32/bin/PlusServer.exe --config-file="/c/Users/SBN/Code/skullbasenavigation/PLUS_settings/PlusDeviceSet_Server_StealthLinkTracker_pyIGTLink.xml" --verbose=4
 $SHELL
